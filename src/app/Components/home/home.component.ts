1595cb6bb3b6c4111790c67ca8118a23a316ff86
Vitalis Kirui <vitaliskirui1@gmail.com>
2023-03-02 11:04:07 +0300

    creating functionality for proceed to verify function

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
+
+  // Proceed to verify function
+  verifyform() {
+
+    this.router.navigate(['verify']);
+    
+  }
 
   ngOnInit(): void {
   }
