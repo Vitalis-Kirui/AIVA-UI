2e9d9b0f0e32b80df56b7b481b0d3f0014659886
Vitalis Kirui <vitaliskirui1@gmail.com>
2023-03-02 11:19:21 +0300

    creating verify staff number function

--- src/app/Components/verification/verification.component.ts
+++ src/app/Components/verification/verification.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-verification',
@@ -7,7 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerificationComponent implements OnInit {
 
-  constructor() { }
+  // Success verification
+  success : boolean = false;
+
+  constructor(private router:Router) { }
+
+  // Verify function
+  verifystaffnumber() {
+
+    this.success = true;
+    this.router.navigate(['home']);
+    
+  }
 
   ngOnInit(): void {
   }
