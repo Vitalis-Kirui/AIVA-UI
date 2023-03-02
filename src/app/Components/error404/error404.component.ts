10a70904666bf2e4878134d60a416cd4a579389f
Vitalis Kirui <vitaliskirui1@gmail.com>
2023-03-02 11:33:31 +0300

    creating error 404 page and a redirect button to menu

--- src/app/Components/error404/error404.component.ts
+++ src/app/Components/error404/error404.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-error404',
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Error404Component implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
+
+  // Go back home
+  backtomenu() {
+
+    this.router.navigate(['menu']);
+    
+  }
 
   ngOnInit(): void {
   }
