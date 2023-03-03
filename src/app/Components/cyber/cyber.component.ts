a6f5007ddf3a857488e8c907dcceddd0a2ce38e3
Vitalis Kirui <vitaliskirui1@gmail.com>
2023-03-03 20:13:42 +0300

    validating client's registry form

--- src/app/Components/cyber/cyber.component.ts
+++ src/app/Components/cyber/cyber.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cyber',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CyberComponent implements OnInit {
 
-  constructor() { }
+  // Forms variables
+  clientsform!: FormGroup;
 
-  ngOnInit(): void {
+  // Display variables
+  pastclientsname: boolean = false;
+
+  constructor(private fbservice : FormBuilder) { }
+
+  ngOnInit() {
+
+    // Client's form model
+    this.clientsform = this.fbservice.group({
+      clientsname:['', [Validators.required, Validators.minLength(3)]]
+    })
+
+  }
+
+  // Getter functions
+
+  // Client's name
+  get clientsname() {
+    return this.clientsform.get('clientsname');
+  }
+
+  // Submit client's form function
+  registerclient() {
+
+    this.pastclientsname = true;
+
+    console.log(this.clientsform.value);
+
   }
 
 }
