18bde8c994790cf43873ac4ca71c854e14fc9940
Vitalis Kirui <vitaliskirui1@gmail.com>
2023-03-03 11:54:04 +0300

    creating what to display logic

--- src/app/Components/staffs/staffs.component.ts
+++ src/app/Components/staffs/staffs.component.ts
@@ -7,7 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StaffsComponent implements OnInit {
 
+  // Display variables
+  seeexistingstaffs: boolean = false;
+  addnewstaff: boolean = false;
+
   constructor() { }
+
+  // Existing staff function
+  existingstaffs() {
+    
+    this.seeexistingstaffs = true;
+    this.addnewstaff = false;
+
+  }
+
+  // Add new staff function
+  addstaff() {
+
+    this.addnewstaff = true;
+    this.seeexistingstaffs = false;
+    
+  }
 
   ngOnInit(): void {
   }
