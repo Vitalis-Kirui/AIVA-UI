6e08fa0dc7a230dec2a9c93447a527c7d43369af
Vitalis Kirui <vitaliskirui1@gmail.com>
2023-03-04 11:45:59 +0300

    adding payments methods to cyber form model and their validations

--- src/app/Components/cyber/cyber.component.ts
+++ src/app/Components/cyber/cyber.component.ts
@@ -30,8 +30,44 @@ export class CyberComponent implements OnInit {
     this.cyberform = this.fbservice.group({
       name: ['', [Validators.required]],
       quantity: [''],
-      totalcost: ['', [Validators.required]]
+      totalcost: ['', [Validators.required]],
+      mpesa: [''],
+      cash: [''],
+      cheque: [''],
+      code: ['']
     });
+
+     // M-Pesa conditional validation
+  this.cyberform.get('mpesa')?.valueChanges
+     .subscribe((checkedvalue) =>{
+
+      // getting code field
+      const code = this.code;
+
+      if(checkedvalue){
+        code?.setValidators([Validators.required]);
+      }
+      else{
+        code?.clearValidators()
+      }
+      code?.updateValueAndValidity();
+     }) 
+    
+    // Cheque conditional validation
+    this.cyberform.get('cheque')?.valueChanges
+     .subscribe((checkedvalue) =>{
+
+      // getting code field
+      const code = this.code;
+
+      if(checkedvalue){
+        code?.setValidators([Validators.required]);
+      }
+      else{
+        code?.clearValidators()
+      }
+      code?.updateValueAndValidity();
+     }) 
 
   }
 
@@ -55,6 +91,26 @@ export class CyberComponent implements OnInit {
   // Total cost
   get totalcost() {
     return this.cyberform.get('totalcost');
+  }
+
+  // M-pesa
+  get mpesa() {
+    return this.cyberform.get('mpesa');
+  }
+
+  // cash
+  get cash() {
+    return this.cyberform.get('cash');
+  }
+
+  // cheque
+  get cheque() {
+    return this.cyberform.get('cheque');
+  }
+
+  // Code 
+  get code() {
+    return this.cyberform.get('code');
   }
 
 
