625dbda8849068afec0980f66a9bebbad3003091
Vitalis Kirui <vitaliskirui1@gmail.com>
2023-03-10 19:33:23 +0300

    navigation to full reports and back to main menu

--- src/app/Components/reports/reports.component.ts
+++ src/app/Components/reports/reports.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-reports',
@@ -7,7 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
+  
+  // sales full report
+  salesfullreport() {
+    this.router.navigate(['sales/full-report']);
+  }
+
+  // Expenses full report
+  expensesfullreport() {
+    this.router.navigate(['expenses/full-report']);
+  }
+
+  // Cyber full report
+  cyberfullreport() {
+    this.router.navigate(['cyber/full-report']);
+  }
+
+  // Go back function
+  goback() {
+    this.router.navigate(['menu']);
+  }
 
   ngOnInit(): void {
   }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddingComponent } from './Components/adding/adding.component';
+import { CyberFullComponent } from './Components/cyber-full/cyber-full.component';
 import { CyberComponent } from './Components/cyber/cyber.component';
 import { Error404Component } from './Components/error404/error404.component';
+import { ExpensesFullComponent } from './Components/expenses-full/expenses-full.component';
 import { ExpensesComponent } from './Components/expenses/expenses.component';
 import { HomeComponent } from './Components/home/home.component';
 import { MenuComponent } from './Components/menu/menu.component';
 import { ReportsComponent } from './Components/reports/reports.component';
+import { SalesFullComponent } from './Components/sales-full/sales-full.component';
 import { SalesComponent } from './Components/sales/sales.component';
 import { StaffsComponent } from './Components/staffs/staffs.component';
 import { VerificationComponent } from './Components/verification/verification.component';
@@ -21,6 +24,9 @@ const routes: Routes = [
   { path: 'sales', component: SalesComponent },
   { path: 'staffs', component: StaffsComponent },
   { path: 'menu', component: MenuComponent },
+  { path: 'sales/full-report', component: SalesFullComponent },
+  {path: 'expenses/full-report', component: ExpensesFullComponent},
+  {path: 'cyber/full-report', component: CyberFullComponent},
   { path: '**', component: Error404Component },
 ];
 
