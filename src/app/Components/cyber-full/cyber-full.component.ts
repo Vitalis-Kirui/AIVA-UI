3824b2b09ead5f866924eadbe75a480729060075
Vitalis Kirui <vitaliskirui1@gmail.com>
2023-03-10 19:40:49 +0300

    creating cyber full report and back to reports function

--- src/app/Components/cyber-full/cyber-full.component.ts
+++ src/app/Components/cyber-full/cyber-full.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cyber-full',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CyberFullComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
+  
+  // Back to reports function
+  backtoreports() {
+    this.router.navigate(['reports']);
+  }
 
   ngOnInit(): void {
   }
