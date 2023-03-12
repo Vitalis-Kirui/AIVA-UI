9d67b3ea7e605761a3f9ea224ec3d3854d9b667e
Vitalis Kirui <vitaliskirui1@gmail.com>
2023-03-12 12:31:11 +0300

    consuming endpoints at cyber services service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -37,8 +38,9 @@ import { SalesFullComponent } from './Components/sales-full/sales-full.component
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     ReactiveFormsModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,35 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  // Stock endpoints
+  addstock: 'http://localhost:3000/stock/new-stock',
+  allstock: 'http://localhost:3000/stock/all-stocks',
+  
+  // Cyber endpoints
+  registercyberservice: 'http://localhost:3000/cyber/new-cyber-service',
+  allcyberservices:'http://localhost:3000/cyber/all-cyber-services',
+  todaycyberservices: 'http://localhost:3000/cyber/today-cyber-service',
+  
+  // Expenses endpoints
+  newexpense:'http://localhost:3000/expenses/new-expense',
+  allexpenses:'http://localhost:3000/expenses/all-expenses',
+  todayexpenses: 'http://localhost:3000/expenses/today-expenses',
+  
+  // Sales endpoints
+  newsale:'http://localhost:3000/sales/new-sale',
+  allsales:'http://localhost:3000/sales/all-sales',
+  todaysales: 'http://localhost:3000/sales/today-sales',
+  
+  // Staffs endpoints
+  newstaff:'http://localhost:3000/staffs/new-staff',
+  allstaffs:'http://localhost:3000/staffs/all-staffs',
+  ceostaffs: 'http://localhost:3000/staffs/ceos',
+  managementstaffs:'http://localhost:3000/staffs/management',
+  supervisorstaffs:'http://localhost:3000/staffs/supervisors',
+  attendantstaffs:'http://localhost:3000/staffs/attendants',
+  
 };
 
 /*
