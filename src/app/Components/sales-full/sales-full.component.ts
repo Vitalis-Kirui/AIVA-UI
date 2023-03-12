2871add0971d28256a0e14293e11dc06287f8ad7
Vitalis Kirui <vitaliskirui1@gmail.com>
2023-03-12 17:36:40 +0300

    fetching and displaying all sales

--- src/app/Components/sales-full/sales-full.component.ts
+++ src/app/Components/sales-full/sales-full.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { SaleService } from 'src/app/Services/sale.service';
 
 @Component({
   selector: 'app-sales-full',
@@ -8,13 +9,28 @@ import { Router } from '@angular/router';
 })
 export class SalesFullComponent implements OnInit {
 
-  constructor(private router : Router) { }
+  // Sales array
+  sales : any = [];
+
+  constructor(private router : Router, private salesservice : SaleService) { }
 
     // Back to reports function
   backtoreports() {
     this.router.navigate(['reports']);
   }
-  ngOnInit(): void {
+  ngOnInit() {
+
+    // All sales
+    this.salesservice.getallsales()
+      .subscribe(data => {
+        this.sales = data.sales;
+        console.log(this.sales);
+      },
+        error => {
+          console.log(error);
+        }
+      )
+
   }
 
 }
