bae102d93064bfed90b234fc3acfd4e4c1e44af2
Vitalis Kirui <vitaliskirui1@gmail.com>
2023-03-12 12:40:10 +0300

    consuming all stock endpoints in stock service

--- src/app/Services/stock.service.ts
+++ src/app/Services/stock.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StockService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // Adding new stock
+  addnewstock(stock: any) {
+    return this.http.post<any>(environment.addstock, stock);
+  }
+
+  // Getting all the stocks
+  getallstock(): Observable<any>{
+    return this.http.get<any>(environment.allstock);
+  }
+  
 }
