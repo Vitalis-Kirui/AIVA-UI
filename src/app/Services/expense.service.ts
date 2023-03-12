354ba3f5f799358950d45848e42a766b30f481ed
Vitalis Kirui <vitaliskirui1@gmail.com>
2023-03-12 12:34:54 +0300

    consuming expenses endpoints on expense service

--- src/app/Services/expense.service.ts
+++ src/app/Services/expense.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExpenseService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // registering new expense
+  registernewexpense(expense: any) {
+    return this.http.post<any>(environment.newexpense, expense);
+  }
+
+  // Get all the expenses
+  getallexpenses(): Observable<any>{
+    return this.http.get<any>(environment.allexpenses);
+  }
+
+  // Getting today's expenses
+  gettodayexpenses(): Observable<any> {
+    return this.http.get<any>(environment.todayexpenses);
+  }
+  
 }
