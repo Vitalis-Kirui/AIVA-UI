1f8e6888ce08cfe90f9628c047894a3206dc7322
Vitalis Kirui <vitaliskirui1@gmail.com>
2023-03-16 22:03:39 +0300

    creating staff update form and form model

--- src/app/Components/update-staff/update-staff.component.ts
+++ src/app/Components/update-staff/update-staff.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { StaffService } from 'src/app/Services/staff.service';
 
 @Component({
   selector: 'app-update-staff',
@@ -7,9 +9,110 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateStaffComponent implements OnInit {
 
-  constructor() { }
+   // Form variable
+  newstaffform!: FormGroup;
 
-  ngOnInit(): void {
+  // Work stations
+  stations: any = ['All', 'Chebunyo', 'Kaboson']
+
+  // Gender
+  genders: any = ['Male', 'Female']
+
+  // Roles
+  roles: any = ['CEO', 'Management', 'Supervisor', 'Attendant']
+
+  constructor(private fbservice:FormBuilder, private staffservice:StaffService) { }
+
+  ngOnInit() {
+
+    // Form model
+    this.newstaffform = this.fbservice.group({
+      firstname: ['', [Validators.required, Validators.minLength(3)]],
+      secondname: ['', [Validators.required, Validators.minLength(3)]],
+      nationalid: ['', [Validators.required, Validators.minLength(6)]],
+      phonenumber: ['', [Validators.required, Validators.minLength(9)]],
+      age: ['', [Validators.required]],
+      image: [''],
+      role: ['', [Validators.required]],
+      gender: ['', [Validators.required]],
+      workstation: ['', [Validators.required]],
+      monthlysalary: ['', [Validators.required]]
+    })
+
   }
 
+  // Update staff function
+  updatestaff() {
+    
+  }
+
+  // GETTER FUNCTIONS
+
+  // Firstname
+  get firstname() {
+    return this.newstaffform.get('firstname');
+  }
+
+  // Second name
+  get secondname() {
+    return this.newstaffform.get('secondname');
+  }
+
+  // id
+  get id() {
+    return this.newstaffform.get('nationalid');
+  }
+
+  // Phone number
+  get phonenumber() {
+    return this.newstaffform.get('phonenumber');
+  }
+
+  // age
+  get age() {
+    return this.newstaffform.get('age');
+  }
+
+  // role
+  get role() {
+    return this.newstaffform.get('role');
+  }
+
+  // gender
+  get gender() {
+    return this.newstaffform.get('gender');
+  }
+
+  // Workstation
+  get workstation() {
+    return this.newstaffform.get('workstation');
+  }
+
+  // Salary
+  get salary() {
+    return this.newstaffform.get('monthlysalary');
+  }
+
+  // Tracking changes in the dropdowns
+
+  // Role
+  changerank(event: any) {
+    this.role?.setValue(event.target.value, {
+      onlySelf: true,
+    });
+  }
+
+  // Gender
+  changegender(event: any) {
+    this.gender?.setValue(event.target.value, {
+      onlySelf: true,
+    });
+  }
+
+  // Station
+  changestation(event: any) {
+    this.workstation?.setValue(event.target.value, {
+      onlySelf: true,
+    });
+  }
 }
