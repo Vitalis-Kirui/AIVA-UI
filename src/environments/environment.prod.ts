2ad4f9639ed24a26e70090a520c061bef545a0d1
Vitalis Kirui <vitaliskirui1@gmail.com>
2023-11-02 14:38:41 +0300

    updates 2"

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,43 @@
 export const environment = {
-  production: true
+  production: true,
+
+  // Stock endpoints
+  addstock: 'https://aiva-bookshop.cyclic.app/stock/new-stock',
+  allstock: 'https://aiva-bookshop.cyclic.app/stock/all-stocks',
+  singlestock: 'https://aiva-bookshop.cyclic.app/stock/single-stock/',
+  deletestock: 'https://aiva-bookshop.cyclic.app/stock/delete-stock/',
+  updatestock: 'https://aiva-bookshop.cyclic.app/stock/update-stock/',
+
+  
+  // Cyber endpoints
+  registercyberservice: 'https://aiva-bookshop.cyclic.app/cyber/new-cyber-service',
+  allcyberservices:'https://aiva-bookshop.cyclic.app/cyber/all-cyber-services',
+  cyberbydate: 'https://aiva-bookshop.cyclic.app/cyber/by-date?date=',
+  todaycyberservices: 'https://aiva-bookshop.cyclic.app/cyber/today-cyber-services',
+  
+  // Expenses endpoints
+  newexpense:'https://aiva-bookshop.cyclic.app/expenses/new-expense',
+  allexpenses:'https://aiva-bookshop.cyclic.app/expenses/all-expenses',
+  todayexpenses: 'https://aiva-bookshop.cyclic.app/expenses/today-expenses',
+  expensesbydate: 'https://aiva-bookshop.cyclic.app/expenses/by-date?date=',
+
+  
+  // Sales endpoints
+  newsale:'https://aiva-bookshop.cyclic.app/sales/new-sale',
+  allsales:'https://aiva-bookshop.cyclic.app/sales/all-sales',
+  todaysales: 'https://aiva-bookshop.cyclic.app/sales/today-sales',
+  salesbydate: 'https://aiva-bookshop.cyclic.app/sales/by-date?date=',
+  
+  // Staffs endpoints
+  newstaff:'https://aiva-bookshop.cyclic.app/staffs/new-staff',
+  allstaffs:'https://aiva-bookshop.cyclic.app/staffs/all-staffs',
+  ceostaffs: 'https://aiva-bookshop.cyclic.app/staffs/ceos',
+  managementstaffs:'https://aiva-bookshop.cyclic.app/staffs/management',
+  supervisorstaffs:'https://aiva-bookshop.cyclic.app/staffs/supervisors',
+  attendantstaffs: 'https://aiva-bookshop.cyclic.app/staffs/attendants',
+  singlestaff: 'https://aiva-bookshop.cyclic.app/staffs/staff/',
+  deletestaff: 'https://aiva-bookshop.cyclic.app/staffs/staff/',
+  updatestaff: 'https://aiva-bookshop.cyclic.app/staffs/staff/',
+  startsession: 'https://aiva-bookshop.cyclic.app/staffs/start-session?nationalid='
+
 };
